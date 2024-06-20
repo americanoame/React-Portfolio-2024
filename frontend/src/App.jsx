da3f9c2b42508a1938e35c99b55a471ef7f08132
Americano.Dev <andrenations@gmail.com>
2024-06-20 16:28:22 -0400

    Set up React Router with Hero components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
+import Hero from './components/hero/Hero';
 
 const App = () => {
   return (
 <BrowserRouter>
   <main>
     <Navbar />
+    <section id="hero">
+          <Hero />
+        </section>
   </main>
 </BrowserRouter>
   )
