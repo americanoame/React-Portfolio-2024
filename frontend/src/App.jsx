95901b6a8bd92237320e644f89d44580c20421ea
Americano.Dev <andrenations@gmail.com>
2024-06-20 22:23:52 -0400

    Add Project, and Contact sections with appropriate IDs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,26 @@
 import { BrowserRouter } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
 import Hero from './components/hero/Hero';
+import Project from './pages/project/Project';
+import Contact from './pages/contact/Contact';
 
 const App = () => {
   return (
-<BrowserRouter>
-  <main>
-    <Navbar />
-    <section id="hero">
+    <BrowserRouter>
+      <main>
+        <Navbar />
+        <section id="hero">
           <Hero />
         </section>
-  </main>
-</BrowserRouter>
-  )
-}
+        <section id="project">
+          <Project />
+        </section>
+        <section id="contact">
+          <Contact />
+        </section>
+      </main>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
