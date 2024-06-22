5106bee5e647a6329259b2be2c9511a6e66f806e
Americano.Dev <andrenations@gmail.com>
2024-06-22 16:00:57 -0400

    update the base

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/React-Portfolio-2024",
+  base: "/frontend",
   plugins: [react()],
 })
